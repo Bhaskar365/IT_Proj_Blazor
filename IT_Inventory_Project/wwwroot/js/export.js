8cf559da97453250cac1ec7f4e2c1fdedd160e06
bdas615 <bdas@brandinstitute.com>
2023-09-22 20:26:50 +0530

    added excel export function. Work in progress

---
+++ IT_Inventory_Project/wwwroot/js/export.js
@@ -0,0 +1,26 @@
+﻿window.BlazorDownloadFile = (fileName, data) => {
+    const blob = base64toBlob(data, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    document.body.appendChild(a);
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(url);
+};
+
+function base64toBlob(base64, contentType) {
+    const byteCharacters = atob(base64);
+    const byteArrays = [];
+    for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+        const slice = byteCharacters.slice(offset, offset + 512);
+        const byteNumbers = new Array(slice.length);
+        for (let i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+        const byteArray = new Uint8Array(byteNumbers);
+        byteArrays.push(byteArray);
+    }
+    const blob = new Blob(byteArrays, { type: contentType });
+    return blob;
+}
