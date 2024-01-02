a2152679babce9f5a56e5a5515d0c60ed80aad2b
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-01-02 21:53:07 +0530

    piegraph razor page code changes , work in progress

---
+++ IT_Inventory_Project/wwwroot/chart.js
@@ -0,0 +1,14 @@
+﻿//window.setup = (id, config) => {
+//    var ctx = document.getElementById(id).getContext('2d');
+//    new Chart(ctx, config);
+//}
+
+window.setup = (id, config) => {
+    let canvas = document.getElementById(id);
+    if (canvas) {
+        let ctx = canvas.getContext('2d');
+        new Chart(ctx, config);
+    } else {
+        console.error(`Element with id ${id} not found`);
+    }
+};
