f97844a8c215c3635c35bf8b7e8b8efd2ec6a85c
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-03-28 19:18:10 +0530

    chartjs not working even after injecting with interop

--- IT_Inventory_Project/wwwroot/chart.js
+++ IT_Inventory_Project/wwwroot/chart.js
@@ -12,3 +12,19 @@ window.setup = (id, config) => {
         console.error(`Element with id ${id} not found`);
     }
 };
+
+//function createBarChart(canvasId, chartData) {
+//    const ctx = document.getElementById(canvasId).getContext('2d');
+//    new Chart(ctx, JSON.parse(chartData));
+//}
+
+function createBarChart(canvasId, chartData) {
+    const ctx = document.getElementById(canvasId)
+    if (ctx) {
+        let canvas = ctx.getContext('2d');
+        new Chart(ctx, JSON.parse(chartData));
+    }
+    else {
+        console.error(`Element with id ${id} not found`);
+    }
+}
